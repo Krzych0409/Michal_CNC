be6f1bd17588569f03424cc9fb7f850c5ae970d5
Krzych0409 <krzychmich@onet.pl>
2023-02-03 13:50:23 +0100

    Klawiatura 4 przyciski, dodanie rozpędzania

--- main.py
+++ main.py
@@ -25,22 +25,26 @@ def kliknij(zdjecie):
 
 
 def akcja(klawisz):
-    if klawisz == keyboard.Key.esc:
-        return False
-    elif klawisz == keyboard.Key.left:
+    if klawisz == keyboard.Key.media_previous:
         print(f'Stop - {klawisz}', end='')
         kliknij('stop.png')
-    elif klawisz == keyboard.Key.right:
-        print(f'Start - {klawisz}', end='')
-        kliknij('start.png')
-    elif klawisz == keyboard.Key.down:
+    elif klawisz == keyboard.Key.media_next:
         print(f'Do zera - {klawisz}', end='')
         kliknij('dozera.png')
+    elif klawisz == keyboard.Key.media_volume_down:
+        print(f'Start - {klawisz}', end='')
+        kliknij('start.png')
+    elif klawisz == keyboard.Key.media_volume_up:
+        print(f'Rozpędzanie - {klawisz}', end='')
+        kliknij('rozpedzanie.png')
+    elif klawisz == keyboard.Key.esc:
+        print(f'Zamknięcie programu - {klawisz}')
+        return False
     else:
         print('Odpuszczony klawisz: {}'.format(klawisz))
 
 
-
+# Utworzenie 'nasłuchiwacza'
 with keyboard.Listener(on_release=akcja) as listener:
     listener.join()
 
